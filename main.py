a9a5b180baa1612eec28820734f4b5d72cc480b2
ali <saidomarali@gmail.com>
2018-12-18 15:36:36 +0100

    data solution

--- main.py
+++ main.py
@@ -113,7 +113,9 @@ Shanti Saucier"""
 
 NAMES = USERS.split()
 MAX_END_INT = 2000
-
+MAX_PLAYER = 100
+MAX_PARTIE_PAR_JEUX = 10
+MAX_JOUEUR_PAR_ROOM = 10
 
 def generate_name(nb_name=1):
     result = list()
@@ -170,7 +172,7 @@ def convert_list_to_str(*lists, sep=","):
     return sep.join([repr(str(s)) for s in result if s])
 
 
-def generate_player(nb_player=10000):
+def generate_player(nb_player=MAX_PLAYER):
     heade_tab = ["id_player", "speudo", "mdp", "prenom", "nom", "date_naissance", "sexe"]
     for i in range(1, nb_player):
         names = generate_name(nb_name=4)
@@ -178,25 +180,57 @@ def generate_player(nb_player=10000):
         from_date = (datetime.datetime.now() - datetime.timedelta(days=365 * random_age))
         date_ = generate_date(nb_date=1, from_date=from_date)
         # score = generate_int(start=1, end=10, nb_int=1)
-        sexe = random.choice(["M", "F", "NA"])
+        sexe = random.choice(["M", "F"])
         row = convert_list_to_str([i], names, date_, sexe, sep="\t")
         print(row)
 
 
-def generate_partie(nb_player=100):
+def generate_partie(nb_jeu=871):
+    ids_room = list()
     heade_tab = ["id_partie", "id_room", "date_start", "date_end"]
-    for i in range(1, nb_player):
-        # names = generate_name(nb_name=2)
-        id_room = random.randint(1, i or 2)
-        date_s = generate_date(nb_date=2)
-        # score = generate_int(start=1, end=10, nb_int=1)
-        row = convert_list_to_str([i], [id_room], date_s, sep="\t")
-        print(row)
+    part_file = open("partie.csv", mode="w")
+    for i in range(1, nb_jeu):
+        nb_partie = random.randint(1, MAX_PARTIE_PAR_JEUX)
+        for j in range(1, nb_partie):
+            # names = generate_name(nb_name=2)
+            id_room = "%d%d" % (i, j)
+            date_s = generate_date(nb_date=2)
+            # score = generate_int(start=1, end=10, nb_int=1)
+            row = convert_list_to_str([id_room], date_s, sep="\t")
+            ids_room.append(str(id_room))
+            print(row, file=part_file)
+    return ids_room
+
+
+def generate_room(ids_room):
+
+    ids_joueurs = list(range(1, MAX_PLAYER))
+    file_room = open("room.csv", mode="w")
+    file_score = open("score.csv", mode="w")
+
+    for partie_i, id_room in enumerate(ids_room, start=1):
+        nb_joueur = random.randint(1, MAX_JOUEUR_PAR_ROOM)
+        copy_ids_joueurs = ids_joueurs[:]
+        for j in range(nb_joueur):
+            try:
+                random_index_joueur = random.choice(copy_ids_joueurs)
+                copy_ids_joueurs.remove(random_index_joueur)
+            except:
+                random_index_joueur = 1
+
+            row = convert_list_to_str([id_room], [random_index_joueur], [nb_joueur], sep="\t")
+            print(row, file=file_room)
+
+            score = random.randint(1, nb_joueur*1000)
+            ranking = random.randint(1, nb_joueur)
+            row_ranking = convert_list_to_str(
+                [random_index_joueur], [partie_i], [score], [ranking], sep="\t")
+            print(row_ranking, file=file_score)
 
 
 def main():
-    generate_player()
-
+    ids_room = generate_partie()
+    generate_room(ids_room)
 
 
 if __name__ == '__main__':
--- solution.sql
+++ solution.sql
@@ -15,8 +15,8 @@ CREATE TABLE joueur(
 
 );
 
-DROP TABLE IF EXISTS scrore;
-CREATE TABLE scrore(
+DROP TABLE IF EXISTS score;
+CREATE TABLE score(
   id_joueur integer,
   id_partie integer,
   score integer,
@@ -35,7 +35,7 @@ CREATE TABLE editeur(
 DROP TABLE IF EXISTS room ;
 CREATE TABLE room(
   id_room integer,
-  id_joueyr integer,
+  id_joueur integer,
   nb_joueur integer
 
 );
@@ -43,7 +43,6 @@ CREATE TABLE room(
 DROP TABLE IF EXISTS partie ;
 CREATE TABLE partie(
   id_partie integer primary key AUTO_INCREMENT,
-  id_joueur integer,
   id_room integer,
   date_debut datetime,
   date_fin datetime
@@ -56,7 +55,59 @@ CREATE TABLE jeu(
   nom varchar(50),
   id_editeur integer,
   date_sortie date,
-  typ_jeu varchar(5),
+  type_jeu varchar(5),
   pegi integer
 );
 
+-- nom	genre	editeur	date_sortie
+USE GAMES
+DROP TABLE IF EXISTS jeu_l ;
+CREATE TABLE jeu_l(
+  id_jeu integer primary key AUTO_INCREMENT ,
+  nom varchar(255),
+  type_jeu varchar(60),
+  editeur varchar(200),
+  date_sortie varchar(30)
+);
+
+LOAD DATA LOCAL INFILE "/Users/alisaidomar/generate_sql_data/game_new.txt"
+  INTO TABLE jeu_l
+  COLUMNS TERMINATED BY "\t"
+  OPTIONALLY ENCLOSED BY "'"
+  LINES TERMINATED BY "\n"
+  IGNORE 1 LINES
+  (nom, type_jeu, editeur, date_sortie);
+
+TRUNCATE TABLE joueur;
+LOAD DATA LOCAL INFILE "/Users/alisaidomar/generate_sql_data/player.csv"
+  INTO TABLE joueur
+  COLUMNS TERMINATED BY "\t"
+  OPTIONALLY ENCLOSED BY "'"
+  LINES TERMINATED BY "\n"
+
+
+TRUNCATE TABLE score;
+LOAD DATA LOCAL INFILE "/Users/alisaidomar/generate_sql_data/score.csv"
+  INTO TABLE score
+  COLUMNS TERMINATED BY "\t"
+  OPTIONALLY ENCLOSED BY "'"
+  LINES TERMINATED BY "\n"
+
+
+TRUNCATE TABLE partie;
+LOAD DATA LOCAL INFILE "/Users/alisaidomar/generate_sql_data/partie.csv"
+  INTO TABLE partie
+  COLUMNS TERMINATED BY "\t"
+  OPTIONALLY ENCLOSED BY "'"
+  LINES TERMINATED BY "\n"
+ (id_room, date_debut, date_fin)
+
+TRUNCATE TABLE room;
+LOAD DATA LOCAL INFILE "/Users/alisaidomar/generate_sql_data/room.csv"
+  INTO TABLE room
+  COLUMNS TERMINATED BY "\t"
+  OPTIONALLY ENCLOSED BY "'"
+  LINES TERMINATED BY "\n"
+
+
+-- calcul to top X
